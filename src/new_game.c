c7926f2c88549130516b40ea7b02888899ea0b26
greenDarkness <imrs555joe@hotmail.com>
2025-11-23 22:12:15 -0500

    Set default options: fast text speed, SET battle style, and L=A button mode

--- src/new_game.c
+++ src/new_game.c
@@ -96,6 +96,7 @@ static void SetDefaultOptions(void)
     gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SET;
     gSaveBlock2Ptr->optionsBattleSceneOff = FALSE;
     gSaveBlock2Ptr->regionMapZoom = FALSE;
+    gSaveBlock2Ptr->optionsButtonMode = OPTIONS_BUTTON_MODE_L_EQUALS_A;
 }
 
 static void ClearPokedexFlags(void)
