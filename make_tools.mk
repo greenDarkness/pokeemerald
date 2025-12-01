a9f16f43b721542bf9dddb4cbccf5b6d28806a8e
Marcus Huderle <huderlem@gmail.com>
2025-12-01 09:01:26 -0600

    Convert .aif files to .wav (#2213)
    * Convert uncompress samples to .wav files, and introduce wav2agb with matching capabilities
    
    Convert cries
    
    Remove aif2pcm and cleanup wav2agb
    
    * Add --no-pad option to support hq mixer
    
    * Update wav2agb README
    
    * Include the alignment padding in wav2agb
    
    * cleanup

--- make_tools.mk
+++ make_tools.mk
@@ -4,35 +4,19 @@
 MAKEFLAGS += --no-print-directory
 
 # Inclusive list. If you don't want a tool to be built, don't add it here.
-
 TOOLS_DIR := tools
-TOOL_NAMES := aif2pcm bin2c gbafix gbagfx jsonproc mapjson mid2agb preproc ramscrgen rsfont scaninc
+TOOL_NAMES := bin2c gbafix gbagfx jsonproc mapjson mid2agb preproc ramscrgen rsfont scaninc wav2agb
+
 TOOLDIRS := $(TOOL_NAMES:%=$(TOOLS_DIR)/%)
 
 # Tool making doesnt require a pokeemerald dependency scan.
 RULES_NO_SCAN += tools check-tools clean-tools $(TOOLDIRS)
 .PHONY: $(RULES_NO_SCAN)
 
-tools: agbcc $(TOOLDIRS)
+tools: $(TOOLDIRS)
 
 $(TOOLDIRS):
 	@$(MAKE) -C $@
 
 clean-tools:
 	@$(foreach tooldir,$(TOOLDIRS),$(MAKE) clean -C $(tooldir);)
-
-# Clone and build agbcc if not present
-.PHONY: agbcc
-agbcc:
-ifeq (,$(wildcard agbcc))
-	git clone https://github.com/pret/agbcc || true
-endif
-
-ifeq (,$(wildcard agbcc/.rebuilt))
-	cd agbcc && git clean -fX && ./build.sh && ./install.sh ../../pokeemerald
-	touch agbcc/.rebuilt
-else
-ifeq (,$(wildcard tools/agbcc/bin/agbcc$(EXE)))
-	cd agbcc && ./install.sh ../../pokeemerald
-endif
-endif
