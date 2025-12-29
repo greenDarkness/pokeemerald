45fdd03c453d1da57e4e29927b43ba814325e7a8
greenDarkness <imrs555joe@hotmail.com>
2025-12-29 03:03:46 -0500

    Fix daycare lady's granddaughter eggs having mismatched pp

--- src/field_specials.c
+++ src/field_specials.c
@@ -4650,7 +4650,7 @@ void GiveRandomPerfectIVEgg(void)
     // Build list of available egg moves
     for (i = 0; i < EGG_MOVES_ARRAY_COUNT; i++)
     {
-        if (gEggMoves[eggMoveIdx + i] > EGG_MOVES_SPECIES_OFFSET)
+        if (gEggMoves[eggMoveIdx + i] > EGG_MOVES_SPECIES_OFFSET || gEggMoves[eggMoveIdx + i] == MOVE_NONE)
             break;
         eggMoves[numEggMoves++] = gEggMoves[eggMoveIdx + i];
     }
