f45248a7ee319390c2b206f0706f5665ee041d8f
Sidohdon <drforbes02@gmail.com>
2026-01-02 16:05:29 -0500

    Oldale: rival gangs event flags set

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1206,6 +1206,7 @@
 #define FLAG_OLDALE_GRUNT_TAUGHT                                  0x470 // Set when the Oldale grunt has taught COVET
 #define FLAG_ITEM_ROUTE_103_PP_UP                                   0x471
 #define FLAG_UNUSED_0x472                                           0x472 // Unused Flag
+#define FLAG_HIDE_OLDALE_GRUNTS_BURRIED                             0x472
 #define FLAG_ITEM_ROUTE_108_STAR_PIECE                              0x473
 #define FLAG_ITEM_ROUTE_109_POTION                                  0x474
 #define FLAG_ITEM_ROUTE_110_ELIXIR                                  0x475
