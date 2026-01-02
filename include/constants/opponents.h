7aef15afe637694544624430e19d92a3c36c2c52
Sidohdon <drforbes02@gmail.com>
2026-01-02 15:06:08 -0500

    Oldale: rival gangs improved

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -857,6 +857,9 @@
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
 #define TRAINER_GRUNT_AQUA_OLDALE_1         855
+#define TRAINER_GRUNT_AQUA_OLDALE_2         856
+#define TRAINER_GRUNT_MAGMA_OLDALE_1        857
+#define TRAINER_GRUNT_MAGMA_OLDALE_2        858
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
