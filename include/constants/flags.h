d3c9a3a86403503a2602ea6d932165982a802b98
Sidohdon <drforbes02@gmail.com>
2026-01-06 21:47:25 -0500

    Truck Event: added sparkle effect, fixed cabinet not despawning

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1253,7 +1253,7 @@
 #define FLAG_HIDE_MAYSHOUSE_DOLL                                    0x49E // Unused Flag
 #define FLAG_CHOSE_BALTOY                                           0x49F // Unused Flag
 #define FLAG_CHOSE_SHUPPET                                          0x4A0 // Unused Flag
-#define FLAG_UNUSED_0x4A1                                           0x4A1 // Unused Flag
+#define FLAG_HIDE_TRUCKEVENT_CABINET                                0x4A1 // Unused Flag
 #define FLAG_UNUSED_0x4A2                                           0x4A2 // Unused Flag
 #define FLAG_UNUSED_0x4A3                                           0x4A3 // Unused Flag
 #define FLAG_UNUSED_0x4A4                                           0x4A4 // Unused Flag
