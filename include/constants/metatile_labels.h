28cce25ed287cde0b7846d26b8576f06175d3670
Sidohdon <drforbes02@gmail.com>
2026-01-10 05:14:37 -0500

    Route 101: tiles

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -219,7 +219,7 @@
 #define METATILE_General_Grass                 0x001
 #define METATILE_General_Grass_TreeLeft        0x1CE
 #define METATILE_General_Grass_TreeRight       0x1CF
-#define METATILE_General_Grass_TreeUp          0x00E
+#define METATILE_General_Grass_TreeUp          0x1D6
 #define METATILE_General_LongGrass             0x015
 #define METATILE_General_MuddySlope_Frame0     0x0E8
 #define METATILE_General_MuddySlope_Frame1     0x0E9
