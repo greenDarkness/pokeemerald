a73927d941e852e83d2a53f218f785a07578f627
Sidohdon <drforbes02@gmail.com>
2026-01-16 09:52:01 -0500

    Petalburg reflection boy: skeleton

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1264,7 +1264,7 @@
 #define FLAG_ITEM_OLDALE_TOWN_PP_UP                                 0x4A4 // Unused Flag
 #define FLAG_ROUTE103_GAVEBOYPOTION                                 0x4A5 // Unused Flag
 #define FLAG_POTION_BOY_HAS_ITEM                                    0x4A6 // Unused Flag
-#define FLAG_UNUSED_0x4A7                                           0x4A7 // Unused Flag
+#define FLAG_PETALBURG_REFLECTION_BOY                               0x4A7 // Unused Flag
 #define FLAG_UNUSED_0x4A8                                           0x4A8 // Unused Flag
 #define FLAG_UNUSED_0x4A9                                           0x4A9 // Unused Flag
 #define FLAG_UNUSED_0x4AA                                           0x4AA // Unused Flag
