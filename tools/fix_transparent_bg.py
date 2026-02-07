d1311393be4b2961c8c9070fad6b23c09beb4cc1
Sidohdon <drforbes02@gmail.com>
2026-02-07 00:41:45 -0500

    fixed meowth (again)

---
+++ tools/fix_transparent_bg.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+# Load the palette-matched PNG
+img = Image.open('graphics/object_events/pics/pokemon/meowth_npc1pal.png')
+if img.mode != 'P':
+    raise ValueError('Image must be indexed (mode P)')
+
+# Assume the background is the most common color (excluding index 0 if possible)
+from collections import Counter
+pixels = list(img.getdata())
+counts = Counter(pixels)
+
+# If index 0 is not the most common, use the most common as background
+bg_index = counts.most_common(1)[0][0]
+if bg_index != 0:
+    # Remap all bg_index pixels to 0
+    pixels = [0 if p == bg_index else p for p in pixels]
+    img.putdata(pixels)
+    img.save('graphics/object_events/pics/pokemon/meowth_npc1pal_trans.png')
+    print('Wrote graphics/object_events/pics/pokemon/meowth_npc1pal_trans.png with background set to index 0')
+else:
+    print('Background is already index 0, no change made.')
