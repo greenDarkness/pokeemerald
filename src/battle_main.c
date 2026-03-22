313b52631bf596d3b9367b7ce7b63be949370a2b
greenDarkness <imrs555joe@hotmail.com>
2026-03-22 05:54:09 -0400

    Regen PP after a win

--- src/battle_main.c
+++ src/battle_main.c
@@ -48,6 +48,7 @@
 #include "text.h"
 #include "trig.h"
 #include "tv.h"
+#include "field_specials.h"
 #include "util.h"
 #include "window.h"
 #include "constants/abilities.h"
@@ -5451,6 +5452,9 @@ static void HandleEndTurn_FinishBattle(void)
                 }
             }
         }
+
+        if (gBattleOutcome == B_OUTCOME_WON)
+            TryRegeneratePP();
 
         RecordedBattle_SetPlaybackFinished();
         BeginFastPaletteFade(3);
