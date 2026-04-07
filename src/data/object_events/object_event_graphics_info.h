192a2f5ae32ff182ef8a8e3a1ed53ae6ea3d3d35
greenDarkness <imrs555joe@hotmail.com>
2026-04-07 00:43:46 -0400

    Revert Sudowood event to base Emerald sprite

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -4390,20 +4390,20 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Leaf = {
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Sudowoodo = {
     .tileTag = TAG_NONE,
-    .paletteTag = OBJ_EVENT_PAL_TAG_SUDOWOODO,
+    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_3,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
-    .size = 512,
-    .width = 32,
+    .size = 256,
+    .width = 16,
     .height = 32,
-    .paletteSlot = PALSLOT_NPC_SPECIAL,
+    .paletteSlot = PALSLOT_NPC_3,
     .shadowSize = SHADOW_SIZE_M,
     .inanimate = FALSE,
     .disableReflectionPaletteLoad = FALSE,
     .tracks = TRACKS_FOOT,
-    .oam = &gObjectEventBaseOam_32x32,
-    .subspriteTables = sOamTables_32x32,
-    .anims = sAnimTable_Following,
-    .images = sPicTable_Sudowoodo,
+    .oam = &gObjectEventBaseOam_16x32,
+    .subspriteTables = sOamTables_16x32,
+    .anims = sAnimTable_Standard,
+    .images = sPicTable_SudowoodoTree,
     .affineAnims = gDummySpriteAffineAnimTable,
 };
 
