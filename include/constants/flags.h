4e60a4e775d5496c4b4fdb9f8bf49064dd629f92
Sidohdon <drforbes02@gmail.com>
2026-04-10 20:59:41 -0400

    KANTO: Route1 Mart employee item distribution and emote

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -2076,7 +2076,7 @@
 #define FLAG_UNUSED_0x955                           (DAILY_FLAGS_START + 0x35) // Unused Flag
 #define FLAG_UNUSED_0x956                           (DAILY_FLAGS_START + 0x36) // Unused Flag
 #define FLAG_UNUSED_0x957                           (DAILY_FLAGS_START + 0x37) // Unused Flag
-#define FLAG_UNUSED_0x958                           (DAILY_FLAGS_START + 0x38) // Unused Flag
+#define FLAG_DAILY_RECEIVED_ROUTE1_POKEDOLL         (DAILY_FLAGS_START + 0x38)
 #define FLAG_DAILY_ROUTE102_CATCHERBOY              (DAILY_FLAGS_START + 0x39)
 #define FLAG_DAILY_RECEIVED_OLDALE_POKEDOLL         (DAILY_FLAGS_START + 0x3A)
 #define FLAG_UNUSED_0x95B                           (DAILY_FLAGS_START + 0x3B) // Might be used
