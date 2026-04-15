b8184a8bad8ee112b05cd54dc3e687f062372d1c
Sidohdon <drforbes02@gmail.com>
2026-04-15 02:55:55 -0400

    Viridian Forest: added shortcut to Route 3

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1524,3 +1524,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/maps/ViridianCity_PokemonCenter_2F/scripts.inc"
 	.include "data/maps/ViridianCity_School/scripts.inc"
 	.include "data/maps/ViridianForest/scripts.inc"
+
+	.include "data/maps/Route3_SouthEntrance/scripts.inc"
